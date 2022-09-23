e9998f69b5efe07b55396fcb25ae00810a7dea08
Gopal Anjana <gopalanjana7878@gmail.com>
2022-09-23 04:47:22 -0700

    JWT code

---
+++ src/main/java/com/jwt/JwtauthenticationserversApplication.java
@@ -0,0 +1,13 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtauthenticationserversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtauthenticationserversApplication.class, args);
+	}
+
+}
